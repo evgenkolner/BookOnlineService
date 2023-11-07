0c10075b6d647c9f08dada0b6ac8e768a3fd3ed9
evgenkolner <evgen.kolner@gmail.com>
2023-11-07 12:15:13 +0200

    realize repository and service

--- src/main/java/bookstore/model/Category.java
+++ src/main/java/bookstore/model/Category.java
@@ -7,10 +7,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 @Entity
+@NoArgsConstructor
 @Table(name = "categories")
 @SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id = ?")
 @Where(clause = "is_deleted=false")
